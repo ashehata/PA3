fa13c5069f80dc9cdb874e6968f7478a380f0ab9
Aly Shehata <alyshehata@Alys-MacBook-Pro.local>
2014-04-29 22:01:00 -0400

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Aly Shehata on 4/29/14.
+//  Copyright (c) 2014 Aly Shehata. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
